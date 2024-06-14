b4d2bb06107f387a7c3dc80a9659a3c4327f00fd
Michael Heimes <michael@cairndigitalmedia.com>
2024-06-14 11:30:33 -0400

    dev

--- template-parts/part-text-editor.php
+++ template-parts/part-text-editor.php
@@ -5,7 +5,7 @@ $text_editor = get_sub_field('text_editor') ?? null;
 <div class="text-editor module-padding <?php if( !empty($bg_color) ){ echo $bg_color . '-bg'; }?>">
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x align-center">
-			<div class="entry-content cell small-12 large-10 xlarge-9">
+			<div class="entry-content cell small-12 large-10 xlarge-8">
 				<?=wp_kses_post( $text_editor );?>
 			</div>
 		</div>
